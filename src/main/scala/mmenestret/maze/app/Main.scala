3dc5683875b4f8778c79a605c6465719984495fe
Martin MENESTRET <martinmenestret@gmail.com>
2018-09-23 21:44:22 +0200

    Custom monad state instance for F

--- src/main/scala/mmenestret/maze/app/Main.scala
+++ src/main/scala/mmenestret/maze/app/Main.scala
@@ -1,26 +1,28 @@
 package mmenestret.maze.app
 import cats.Monad
-import cats.data.StateT
 import cats.effect.{IO, Sync}
 import cats.implicits._
+import cats.mtl.MonadState
 import mmenestret.maze.ADT._
 import mmenestret.maze.algebras._
 import mmenestret.maze.algebras.impl._
 
 object Main extends App {
 
-  def gameLoop[F[_]: Monad]()(implicit G: GameLogic[F], P: PlayerInteractions[F]): StateT[F, GameState, Unit] =
+  def gameLoop[F[_]: Monad]()(implicit G: GameLogic[F],
+                              P: PlayerInteractions[F],
+                              S: MonadState[F, GameState]): F[Unit] =
     for {
-      state             ← StateT.get[F, GameState]
-      mapRepresentation ← StateT.liftF(G.generateMapRepresentation(state.map))
-      _                 ← StateT.liftF(P.displayMap(mapRepresentation))
-      playerMove        ← StateT.liftF(P.askPlayerDirection(state.layout))
-      gameState         ← StateT.liftF(G.computeGameState(state, playerMove))
+      state             ← S.get
+      mapRepresentation ← G.generateMapRepresentation(state.map)
+      _                 ← P.displayMap(mapRepresentation)
+      playerMove        ← P.askPlayerDirection(state.layout)
+      gameState         ← G.computeGameState(state, playerMove)
       _ ← gameState match {
         case GameState(_, newMap, OnGoing) ⇒
-          StateT.set[F, GameState](gameState.copy(map = newMap): GameState).flatMap(_ ⇒ gameLoop())
+          S.set(gameState.copy(map = newMap): GameState).flatMap(_ ⇒ gameLoop[F]())
         case GameState(_, _, state: Finished) ⇒
-          StateT.liftF[F, GameState, Unit](G.endMessage(state).flatMap(P.displayEndMessage))
+          G.endMessage(state).flatMap(P.displayEndMessage)
       }
     } yield ()
 
@@ -39,10 +41,17 @@ object Main extends App {
       implicit val g: GameLogic[F]           = GameLogicImpl[F]
       implicit val rng: Rng[F]               = RngImp[F]
       implicit val pi: PlayerInteractions[F] = PlayerInteractionsImpl[F]
-      for {
-        initialState ← initiateGame[F]
-        _            ← gameLoop[F]().runA(initialState)
-      } yield ()
+      initiateGame[F].flatMap { initialState ⇒
+        implicit val S: MonadState[F, GameState] = new MonadState[F, GameState] {
+          var internalState: GameState                           = initialState
+          override val monad: Monad[F]                           = Monad[F]
+          override def get: F[GameState]                         = internalState.pure[F]
+          override def set(s: GameState): F[Unit]                = { internalState = s }.pure[F]
+          override def inspect[A](f: GameState ⇒ A): F[A]        = f(internalState).pure[F]
+          override def modify(f: GameState ⇒ GameState): F[Unit] = { internalState = f(internalState) }.pure[F]
+        }
+        gameLoop[F]()
+      }
     }
   }
 
