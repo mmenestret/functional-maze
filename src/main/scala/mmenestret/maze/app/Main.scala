a4618d2fb8df5ba5e464dc5df8f7d9ed7852ca23
Martin MENESTRET <martinmenestret@gmail.com>
2018-09-23 21:09:49 +0200

    separate game loop from game init

--- src/main/scala/mmenestret/maze/app/Main.scala
+++ src/main/scala/mmenestret/maze/app/Main.scala
@@ -9,23 +9,22 @@ import mmenestret.maze.algebras.impl._
 
 object Main extends App {
 
-  def runGame[F[_]](implicit G: GameLogic[F], P: PlayerInteractions[F], R: Rng[F], M: Monad[F]): F[Unit] = {
+  def gameLoop[F[_]: Monad]()(implicit G: GameLogic[F], P: PlayerInteractions[F]): StateT[F, GameState, Unit] =
+    for {
+      state             ← StateT.get[F, GameState]
+      mapRepresentation ← StateT.liftF(G.generateMapRepresentation(state.map))
+      _                 ← StateT.liftF(P.displayMap(mapRepresentation))
+      playerMove        ← StateT.liftF(P.askPlayerDirection(state.layout))
+      gameState         ← StateT.liftF(G.computeGameState(state, playerMove))
+      _ ← gameState match {
+        case GameState(_, newMap, OnGoing) ⇒
+          StateT.set[F, GameState](gameState.copy(map = newMap): GameState).flatMap(_ ⇒ gameLoop())
+        case GameState(_, _, state: Finished) ⇒
+          StateT.liftF[F, GameState, Unit](G.endMessage(state).flatMap(P.displayEndMessage))
+      }
+    } yield ()
 
-    def gameLoop(): StateT[F, GameState, Unit] =
-      for {
-        state             ← StateT.get[F, GameState]
-        mapRepresentation ← StateT.liftF(G.generateMapRepresentation(state.map))
-        _                 ← StateT.liftF(P.displayMap(mapRepresentation))
-        playerMove        ← StateT.liftF(P.askPlayerDirection(state.layout))
-        gameState         ← StateT.liftF(G.computeGameState(state, playerMove))
-        _ ← gameState match {
-          case GameState(_, newMap, OnGoing) ⇒
-            StateT.set[F, GameState](gameState.copy(map = newMap): GameState).flatMap(_ ⇒ gameLoop())
-          case GameState(_, _, state: Finished) ⇒
-            StateT.liftF[F, GameState, Unit](G.endMessage(state).flatMap(P.displayEndMessage))
-        }
-      } yield ()
-
+  def initiateGame[F[_]: Monad](implicit P: PlayerInteractions[F], R: Rng[F]): F[GameState] =
     for {
       _          ← P.clearPlayerScreen()
       sideLength ← P.afkForMapSize()
@@ -33,17 +32,17 @@ object Main extends App {
       layout     ← P.askForKeyboardLayout()
       _          ← P.clearPlayerScreen()
       trapsList  ← R.generateNRngBetween(nbOfTraps)(1, sideLength * sideLength - 1)
-      _          ← gameLoop().runA(GameState.emptyGameState(layout, sideLength, trapsList))
-    } yield ()
-
-  }
+    } yield GameState.emptyGameState(layout, sideLength, trapsList)
 
   def program[F[+ _]: Sync]: F[Unit] = {
     PrintAndReadLanternaImpl.initiate[F].flatMap { implicit term ⇒
       implicit val g: GameLogic[F]           = GameLogicImpl[F]
       implicit val rng: Rng[F]               = RngImp[F]
       implicit val pi: PlayerInteractions[F] = PlayerInteractionsImpl[F]
-      runGame[F]
+      for {
+        initialState ← initiateGame[F]
+        _            ← gameLoop[F]().runA(initialState)
+      } yield ()
     }
   }
 
