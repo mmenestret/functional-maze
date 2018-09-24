28d01965f4bf0528853b7c4e5a317d6ff5f9525c
Martin MENESTRET <martinmenestret@gmail.com>
2018-09-24 10:51:54 +0200

    Getting rid of covariance

--- src/main/scala/mmenestret/maze/ADT/KeyboardLayout.scala
+++ src/main/scala/mmenestret/maze/ADT/KeyboardLayout.scala
@@ -7,6 +7,8 @@ object Qwerty extends KeyboardLayout
 final case class KeyboardKeys(up: Char, down: Char, left: Char, right: Char)
 
 object KeyboardLayout {
+  val azerty: KeyboardLayout = Azerty
+  val qwerty: KeyboardLayout = Qwerty
   def keys(keyboardLayout: KeyboardLayout): KeyboardKeys = keyboardLayout match {
     case Azerty ⇒ KeyboardKeys(up = 'z', down = 's', left = 'q', right = 'd')
     case Qwerty ⇒ KeyboardKeys(up = 'w', down = 's', left = 'a', right = 'd')
