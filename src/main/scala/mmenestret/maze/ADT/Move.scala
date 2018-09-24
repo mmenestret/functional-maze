b2a4f683e668fd0168dbbceb1134d77d58dd6a09
Martin MENESTRET <martinmenestret@gmail.com>
2018-09-24 10:51:54 +0200

    Getting rid of covariance

--- src/main/scala/mmenestret/maze/ADT/Move.scala
+++ src/main/scala/mmenestret/maze/ADT/Move.scala
@@ -1,6 +1,12 @@
 package mmenestret.maze.ADT
 
 sealed abstract class Move
+object Move {
+  val up: Move    = Up
+  val down: Move  = Down
+  val left: Move  = Left
+  val right: Move = Right
+}
 case object Up    extends Move
 case object Down  extends Move
 case object Left  extends Move
